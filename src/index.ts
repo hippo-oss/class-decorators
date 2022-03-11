65c4ca174d2c33900a8ce7289ed67fbdd500ad91
Jesse Myers <jmyers-c@hippo.com>
2022-03-10 17:13:29 -0800

    Add unified decorators as default export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import { composeDecoratorFactories } from '@hippo-oss/dto-decorators';
+import { TRANSFORMER_DECORATORS } from './transformers';
+import { VALIDATOR_DECORATORS } from './validators';
+
+const CLASS_DECORATORS = composeDecoratorFactories([
+    TRANSFORMER_DECORATORS,
+    VALIDATOR_DECORATORS,
+]);
+
+export default CLASS_DECORATORS;
 export * from './transformers';
 export * from './validators';
