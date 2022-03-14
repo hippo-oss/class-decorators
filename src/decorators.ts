4de91abd19e3c8dd64381b6a180612c5c530c61e
Jesse Myers <jmyers-c@hippo.com>
2022-03-14 11:44:48 -0700

    Use structuring export.

--- src/decorators.ts
+++ src/decorators.ts
@@ -7,12 +7,14 @@ export const CLASS_DECORATORS = composeDecoratorFactories([
     VALIDATOR_DECORATORS,
 ]);
 
-export const IsBoolean = CLASS_DECORATORS.IsBoolean;
-export const IsDate = CLASS_DECORATORS.IsDate;
-export const IsDateString = CLASS_DECORATORS.IsDateString;
-export const IsEnum = CLASS_DECORATORS.IsEnum;
-export const IsInteger = CLASS_DECORATORS.IsInteger;
-export const IsNested = CLASS_DECORATORS.IsNested;
-export const IsNumber = CLASS_DECORATORS.IsNumber;
-export const IsString = CLASS_DECORATORS.IsString;
-export const IsUUID = CLASS_DECORATORS.IsUUID;
+export const {
+    IsBoolean,
+    IsDate,
+    IsDateString,
+    IsEnum,
+    IsInteger,
+    IsNested,
+    IsNumber,
+    IsString,
+    IsUUID,
+} = CLASS_DECORATORS;
